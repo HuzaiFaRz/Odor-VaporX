ede30e78a0f25d5494a5d037a1c18753ced01521
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-10-16 11:59:58 +0500

    Framer Motion, Locomotive Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
-import React from "react";
-import s from "./Auth/SignUp";
+import { motion } from "framer-motion";
+
+import LocomotiveScroll from "locomotive-scroll";
+// import { document } from "postcss";
+import { useEffect, useRef } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  useEffect(() => {
+    new LocomotiveScroll({
+      lenisOptions: {
+        syncTouch: true,
+        duration: 1,
+        autoResize: true,
+      },
+    });
+  }, []);
+  return (
+    <main>
+      <div className="page-1 w-full h-[100vh] bg-slate-500 flex justify-center items-center">
+        <motion.div
+          drag
+          dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
+          style={{ cursor: "grab" }}
+          whileDrag={{ scale: 1.5 }}
+          className="w-[400px] h-[500px] bg-zinc-700  text-teal-400 text-5xl flex justify-center items-center"
+        >
+          Huzaifa
+        </motion.div>
+      </div>
+      <div className="page-2 w-full h-[100vh] bg-slate-400"></div>
+      <div className="page-3 w-full h-[100vh] bg-slate-300"></div>
+    </main>
+  );
 };
-
 export default App;
--- src/Auth/SignUp.jsx
+++ src/Auth/SignUp.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import firebaseFile from "./firebase";
 
 const SignUp = () => {
   return <div>SignUp</div>;
